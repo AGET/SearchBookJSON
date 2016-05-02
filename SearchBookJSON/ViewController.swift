a94dcc6471dadd3c8a374b5c6afdb7d3d7c3f7d2
Aldo Gamaliel Estrada Tepec <aldoget1992@gmail.com>
2016-05-01 22:48:09 -0500

    inicio

--- SearchBookJSON/ViewController.swift
+++ SearchBookJSON/ViewController.swift
@@ -8,18 +8,158 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UISearchBarDelegate {
+    
+    @IBOutlet weak var buscar: UISearchBar!
+    @IBOutlet weak var indicador: UIActivityIndicatorView!
+    
+    @IBOutlet weak var titulo: UILabel!
+    @IBOutlet weak var autor1: UILabel!
+    @IBOutlet weak var autor2: UILabel!
+    @IBOutlet weak var autor3: UILabel!
+    
+    @IBOutlet weak var imgPortada: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        autor1.text = ""
+        autor2.text = ""
+        autor3.text = ""
+        titulo.text = ""
+        configureSearchBar()
     }
-
+    
+    
+    
+    // MARK: Configuration
+    func configureSearchBar() {
+        buscar.showsCancelButton = true
+    }
+    
+    // MARK: UISearchBarDelegate
+    func searchBar(searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        NSLog("Selecciono la scope\(selectedScope).")
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        NSLog("Buscando: \(searchBar.text!).")
+        searchBar.resignFirstResponder()
+        busqueda(buscar.text!)
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        NSLog("Cancelado")
+        searchBar.resignFirstResponder()
+    }
+    //SearchEND
+    
+    
+    //Busqueda
+    func busqueda(isbn: String){
+        
+        //AsincronoInicio
+        let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:\(isbn)"
+        NSLog(urls)
+        let url = NSURL(string:urls)
+        let sesion = NSURLSession.sharedSession()
+        
+        let bloque = {(datos:NSData?,resp : NSURLResponse?,error:NSError?) -> Void in
+            if error == nil{
+                if datos?.length > 10 {
+                    dispatch_async(dispatch_get_main_queue(),{
+                        self.optenerDatos(datos!, isbn: isbn)
+                    })
+                }else{
+                    dispatch_async(dispatch_get_main_queue(),{
+                        self.mostrarMensajeSimple("Alerta!", message: "No se encontro", owner: self)
+                        self.indicador.stopAnimating()
+                    })
+                }
+            }else{
+                dispatch_async(dispatch_get_main_queue(),{
+                    self.mostrarMensajeSimple("Alerta!", message: "Error de conexion", owner: self)
+                    self.indicador.stopAnimating()
+                })
+            }
+        }
+        let dt = sesion.dataTaskWithURL(url!, completionHandler: bloque)
+        dt.resume()
+        self.indicador.startAnimating()
+    }
+    //BusquedaFin
+    
+    func mostrarMensajeSimple (title: String, message: String, owner:UIViewController) {
+        let alerta = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alerta.addAction(UIAlertAction(title: "Entiendo", style: UIAlertActionStyle.Default, handler:{ (ACTION:UIAlertAction!)in
+        }))
+        self.presentViewController(alerta, animated: true, completion: nil)
+    }
+    
+    func optenerDatos(dato: NSData, isbn: String){
+        var nombres = [String]()
+        var titulo = ""
+        var img = ""
+        do{
+            let json = try NSJSONSerialization.JSONObjectWithData(dato, options: NSJSONReadingOptions.MutableLeaves)
+            if  let js = json as? NSDictionary{
+                if  let jsIsbn = js["ISBN:\(isbn)"] as? NSDictionary  {
+                    titulo = jsIsbn["title"] as! NSString as String
+                    if let imagenes = jsIsbn["cover"] as? NSDictionary{
+                        img = imagenes["medium"] as! NSString as String
+                        //img = imagenes["small"] as! NSString as String
+                    }
+                    let cantidadAutores = jsIsbn["authors"]?.count
+                    for indice in 0 ..< cantidadAutores!{
+                        let jsAutores = jsIsbn["authors"]![indice] as? NSDictionary
+                        nombres.append(jsAutores!["name"] as! NSString as String)
+                    }
+                }
+            }
+        }
+        catch _ {
+        }
+        self.titulo.text = titulo
+        let num = nombres.count
+        if num > 0 {
+            if num == 1 {
+                self.autor1.text = nombres[0]
+                self.autor2.text = ""
+                self.autor3.text = ""
+            }else if nombres.count == 2 {
+                self.autor1.text = nombres[0]
+                self.autor2.text = nombres[1]
+                self.autor3.text = ""
+            }else if num == 3{
+                self.autor1.text = nombres[0]
+                self.autor2.text = nombres[1]
+                self.autor3.text = nombres[2]
+            }
+        }
+        if !img.isEmpty{
+            if let url = NSURL(string: img) {
+                if let data = NSData(contentsOfURL: url) {
+                    imgPortada.image = UIImage(data: data)
+                }
+            }
+        }else{
+            self.imgPortada.image = UIImage(named: "book")
+        }
+        self.indicador.stopAnimating()
+    }
+    
+    @IBAction func limpiar(sender: AnyObject) {
+        buscar.text = ""
+        titulo.text = ""
+        autor1.text = ""
+        autor2.text = ""
+        autor3.text = ""
+        imgPortada.image = UIImage(named: "book")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
